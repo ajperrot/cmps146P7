cec77244adb1828788372a41b66f8a7de31429f6
ajperrot <alexperrotti@gmail.com>
2018-12-04 00:53:38 -0800

    for zack

--- SHAPE/Assets/Scripts/Properties.cs
+++ SHAPE/Assets/Scripts/Properties.cs
@@ -4,32 +4,70 @@ using UnityEngine;
 
 public class Properties : MonoBehaviour {
 
-				public float latitude;
-				public int id = 0;
-				public float altitude = 0f;
-				public GameObject[] adjacentTiles;
-				public int biome = 0; //enumeration
+	public float latitude;
+	public int id = 0;
+	public float altitude = 0f;
+	public GameObject[] adjacentTiles;
+	public int biome = 0; //enumeration
+	//biomes are alphabetical:
+    // 0 = arctic
+    // 1 = desert
+    // 2 = forest
+    // 3 = jungle
+    // 4 = ocean
+    // 5 = plains
+    // 6 = taiga
 				
 				
 
-				// Use this for initialization
-				void Start()
-				{
-					   latitude = Mathf.Abs(gameObject.transform.position.y);
-				}
-				
-				// Update is called once per frame
-				void Update()
-				{
+	// Use this for initialization
+	void Start()
+	{
+		latitude = Mathf.Abs(gameObject.transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
 
-				}
+	}
 
-				public void changeAltitude(float newAlt)
-				{
-					   altitude = newAlt;
-								Vector3 scale = gameObject.transform.localScale;
-								//scale function based around scale 2 = sea level
-								scale.z = ((newAlt + 8025) / 4000) * 0.8506508f;
-								gameObject.transform.localScale = scale;
-				}
+	public void changeAltitude(float newAlt)
+	{
+		altitude = newAlt;
+		Vector3 scale = gameObject.transform.localScale;
+		//scale function based around scale 2 = sea level
+		scale.z = ((newAlt + 8025) / 4000) * 0.8506508f;
+		gameObject.transform.localScale = scale;
+	}
+
+    public void changeBiome(int newBiome)
+    {
+        Renderer rend = gameObject.GetComponent<Renderer>();
+        biome = newBiome;
+        switch(newBiome){
+            case 0:
+                rend.material.SetColor("_Color", Color.cyan);
+                break;
+            case 1:
+                rend.material.SetColor("_Color", Color.red);
+                break;
+            case 2:
+                rend.material.SetColor("_Color", Color.green);
+                break;
+            case 3:
+                rend.material.SetColor("_Color", Color.gray);
+                break;
+            case 4:
+                rend.material.SetColor("_Color", Color.blue);
+                break;
+            case 5:
+                rend.material.SetColor("_Color", Color.yellow);
+                break;
+            case 6:
+                rend.material.SetColor("_Color", Color.white);
+                break;
+        }
+    }
+
 }
--- SHAPE/Assets/Scripts/World.cs
+++ SHAPE/Assets/Scripts/World.cs
@@ -12,45 +12,48 @@ public class World : MonoBehaviour {
         float addy = Random.RandomRange(0f, 255f);
 
 		Polyhedron polyhedron = gameObject.GetComponent<Polyhedron>();
-				tiles = polyhedron.tiles;
-				float radius = polyhedron.radius;
-				//set id, altitude, adjacency, and biome for each tile
-				for(int i = 0; i < tiles.Length; i++)
-				{
-					GameObject tile = tiles[i];
-								Properties props = tile.GetComponent<Properties>();
-								//set id
-								props.id = i;
-								//set altitude
-								float x = (tile.transform.rotation.y / Mathf.PI) * 100 * 0.1f + addx;
-								print(x);
-								float y = (tile.transform.position.y / radius) * 30 * 0.1f + addy;
-								print(y);
-								float noise = Mathf.PerlinNoise(x, y);
-								props.changeAltitude((noise * 16000) - 8000);
-								//set adjacency
-								props.adjacentTiles = adjTiles(tile);
-								//set tempurature
+		tiles = polyhedron.tiles;
+		float radius = polyhedron.radius;
+		//set id, altitude, adjacency, and biome for each tile
+		for(int i = 0; i < tiles.Length; i++)
+		{
+			GameObject tile = tiles[i];
+			Properties props = tile.GetComponent<Properties>();
+			//set id
+			props.id = i;
+			//set altitude
+			float x = (tile.transform.rotation.y / Mathf.PI) * 100 * 0.1f + addx;
+			float y = (tile.transform.position.y / radius) * 30 * 0.1f + addy;
+			float noise = Mathf.PerlinNoise(x, y);
+			props.changeAltitude((noise * 16000) - 8000);
+			//set adjacency
+			props.adjacentTiles = adjTiles(tile);
+			//set tempurature
+			//set biome
+			if(props.altitude < 0)
+			{
+				props.changeBiome(4);
+			}
 
-				}
+		}
 	}
 
 	GameObject[] adjTiles(GameObject tile)
 	{
-			int sides;
-			if(tile.GetComponent<PentaPrism>())
-					{
-							//if it has 5 sides
-									sides = 5;
-					}else
-					{
-							//if it has 6 sides
-							sides = 6;
-					}
-					GameObject[] temp = (GameObject[])tiles.Clone();
-					temp.OrderBy(t=>(t.transform.position - tile.transform.position));
-					//skip the closest element (the same one as tile)
-					return temp.Take(sides + 1).Skip(1).Cast<GameObject>().ToArray();
+		int sides;
+		if(tile.GetComponent<PentaPrism>())
+			{
+				//if it has 5 sides
+				sides = 5;
+			}else
+			{
+				//if it has 6 sides
+				sides = 6;
+			}
+		GameObject[] temp = (GameObject[])tiles.Clone();
+		temp.OrderBy(t=>(t.transform.position - tile.transform.position));
+		//skip the closest element (the same one as tile)
+		return temp.Take(sides + 1).Skip(1).Cast<GameObject>().ToArray();
 	}
 	
 }
